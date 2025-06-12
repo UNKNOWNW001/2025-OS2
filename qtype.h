dcfab03e37c611a51d4607aee7969e4b8e33c312
UNKNOWNW001 <unknownwhite01@gmail.com>
2025-06-12 18:57:58 +0900

    기본 타입 정의

--- qtype.h
+++ qtype.h
@@ -2,4 +2,11 @@
 #ifndef QTYPE_H
 #define QTYPE_H
 
+typedef int   Key;
+typedef void* Value;
+typedef struct { Key key; Value value; } Item;
+typedef struct { bool success; Item item; } Reply;
+typedef struct Node node_t;  // stub
+typedef struct Queue Queue;  // stub
+
 #endif // QTYPE_H
